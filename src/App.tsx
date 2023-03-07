fa107fed3a25832e62ba3b2700788be1997a829e
JGM01 <jacobgermanamccray@gmail.com>
2023-03-06 23:29:55 -0500

    made Blurb components reusable, removed MainTopic

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import React, { ReactElement, ReactNode } from 'react';
 import './App.css';
 
 import Navbar from './Navbar';
-import MainTopic from './MainTopic';
 import Blurb from './Blurb/Blurb';
 
 function App() {
   return (
     <>
-      <Blurb heading='Header.' subtitle='text, subtitles.'/>
-      <Blurb heading='Yea.' subtitle='even more stuff.'/>
+      <Blurb heading='iPhone 14 Pro' subtitle='Pro. Beyond.' isWhite={false}/>
+      <Blurb heading='iPhone 14' subtitle='Big and Bigger.' isWhite={true}/>
       <Navbar />
 
     </>
--- src/Blurb/Blurb.tsx
+++ src/Blurb/Blurb.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import './Blurb.css';
 
-function Blurb({heading, subtitle}: {heading: string, subtitle: string }) {
+function Blurb({heading, subtitle, isWhite}: {heading: string, subtitle: string, isWhite: boolean}) {
     return (
-        <div className="container">
-            <h1 className="header">{heading}</h1>
-            <p className="subtitle">{subtitle}</p>
+        <div className={`container${isWhite? '-white' : ''}`}>
+            <h2 className={`header${isWhite? '-white' : ''}`}>{heading}</h2>
+            <p className={`subtitle${isWhite? '-white' : ''}`}>{subtitle}</p>
                 <ul className="links">
                     <li>Learn more {'>'}</li>
                     <li>Buy {'>'}</li>
                 </ul>
+            <img className="image" src="/logo192.png" alt="image"/>
         </div>
     )
 }
